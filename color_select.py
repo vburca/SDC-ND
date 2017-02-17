25c628d86605c6b46d999086e2ea8315f6504641
Vlad Burca <vladburca2005@gmail.com>
2017-02-17 00:52:41 -0800

    Add color_select script

---
+++ color_select.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+# Read in the image
+image = mpimg.imread('images/lanes_test1.jpg')
+
+# Grab the x and y size and make a copy of the image
+ysize = image.shape[0]
+xsize = image.shape[1]
+color_select = np.copy(image)
+
+# Define color selection criteria
+red_threshold = 210
+green_threshold = 210
+blue_threshold = 210
+
+rgb_threshold = [red_threshold, green_threshold, blue_threshold]
+
+# Do a boolean or with the "|" character to identify
+# pixels below the thresholds
+thresholds = (image[:,:,0] < rgb_threshold[0]) \
+            | (image[:,:,1] < rgb_threshold[1]) \
+            | (image[:,:,2] < rgb_threshold[2])
+color_select[thresholds] = [0,0,0]
+plt.imshow(color_select)
+# Display the image
+plt.show()
+
+# Uncomment the following code if you are running the code locally and wish to save the image
+# mpimg.imsave("test-after.jpg", color_select)
