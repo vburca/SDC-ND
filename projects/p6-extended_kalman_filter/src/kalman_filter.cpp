5979058d93a3ff8fce2b72419e8ef53bd6a10981
Vlad Burca <vladburca2005@gmail.com>
2017-07-13 00:21:36 -0700

    Add kalman filter and extended kalman filter implementations

--- projects/p6-extended_kalman_filter/src/kalman_filter.cpp
+++ projects/p6-extended_kalman_filter/src/kalman_filter.cpp
@@ -22,6 +22,10 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,6 +33,11 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+
+  UpdateState(y);
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +45,46 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  // Calculate h(x') = (sqrt(px^2 + py^2), arctan(py/px), (px*vx + py*vy)/norm)
+  MatrixXd hx = MatrixXd(3, 1);
+  float norm = sqrt(px * px + py * py);
+
+  // Check for division by zero - if norm is too close to zero, use Jacobian instead
+  if (norm < 0.0001)
+  {
+    hx = H_ * x_;
+  }
+  else
+  {
+    hx << norm,
+           atan2(py, px),
+           (px * vx + py * vy) / norm;
+  }
+
+  // Measurement update
+  VectorXd y = z - hx;
+  // TODO: Still need to normalize the angle
+
+  UpdateState(y);
+}
+
+void KalmanFilter::UpdateState(const Eigen::VectorXd &y)
+{
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  // New estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
--- projects/p6-extended_kalman_filter/src/kalman_filter.h
+++ projects/p6-extended_kalman_filter/src/kalman_filter.h
@@ -64,6 +64,8 @@ public:
    */
   void UpdateEKF(const Eigen::VectorXd &z);
 
+private:
+  void UpdateState(const Eigen::VectorXd &y);
 };
 
 #endif /* KALMAN_FILTER_H_ */
